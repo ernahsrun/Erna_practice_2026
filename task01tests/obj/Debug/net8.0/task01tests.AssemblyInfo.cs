9e0e517ac8bed63fb20e449f943f68f86b6aa23c
ernasharun <ernasharun@gmail.com>
2026-07-06 22:42:25 +0600

    Fix: успешно настроены xUnit тесты

--- task01tests/obj/Debug/net8.0/task01tests.AssemblyInfo.cs
+++ task01tests/obj/Debug/net8.0/task01tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task01tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+723c7a953802ba808d0fc76b5e42aa04c5950d5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06042c2c289ca40937a384026de82381c0619865")]
 [assembly: System.Reflection.AssemblyProductAttribute("task01tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task01tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
