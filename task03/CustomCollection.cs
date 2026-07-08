2e2be92243868e0a6b04997aaaeae06c5debb511
ernasharun <ernasharun@gmail.com>
2026-07-08 14:18:21 +0600

    Задание 03: Итераторы в C#

---
+++ task03/CustomCollection.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace task03;
+
+public class CustomCollection<T> : IEnumerable<T>
+{
+    private readonly List<T> _items = new();
+
+    public void Add(T item) => _items.Add(item);
+    public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    public IEnumerable GetReverseEnumerator()
+    {
+        for (int i = _items.Count - 1; i >= 0; i--)
+        {
+            yield return _items[i];
+        }
+    }
+
+    public static IEnumerable GenerateSequence(int start, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            yield return start + i;
+        }
+    }
+    public IEnumerable FilterAndSort(Func<T, bool> predicate, Func<T, IComparable> keySelector)
+    {
+        return _items.Where(predicate).OrderBy(keySelector);
+    }
+}
